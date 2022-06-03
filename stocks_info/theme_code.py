42abcd69a855d6b870c9c17c4db8d694c700e0aa
KIS Developers <87407853+koreainvestment@users.noreply.github.com>
2022-06-03 09:50:05 +0900

    들여쓰기 수정

--- stocks_info/theme_code.py
+++ stocks_info/theme_code.py
@@ -4,39 +4,29 @@ import ssl
 import zipfile
 import os
 
-
-
 def get_theme_master_dataframe(base_dir):
 
+    ssl._create_default_https_context = ssl._create_unverified_context
+    urllib.request.urlretrieve("https://new.real.download.dws.co.kr/common/master/theme_code.mst.zip", base_dir + "\\theme_code.zip")
+    os.chdir(base_dir)
 
+    kospi_zip = zipfile.ZipFile('theme_code.zip')
+    kospi_zip.extractall()
+    kospi_zip.close()
 
-ssl._create_default_https_context = ssl._create_unverified_context
+    file_name = base_dir + "\\theme_code.mst"
+    df = pd.DataFrame(columns = ['테마코드', '테마명', '종목코드'])
 
-urllib.request.urlretrieve("https://new.real.download.dws.co.kr/common/master/theme_code.mst.zip", base_dir + "\\theme_code.zip")
+    ridx = 1
+    with open(file_name, mode="r", encoding="cp949") as f:
+        for row in f:
+            tcode = row[0:3]            # 테마코드
+            jcode = row[-10:].rstrip()  # 테마명
+            tname = row[3:-10].rstrip() # 종목코드
+            df.loc[ridx] = [tcode, tname, jcode]
+            print(df.loc[ridx])
+            ridx += 1
 
-os.chdir(base_dir)
+    return df
 
-
-
-kospi_zip = zipfile.ZipFile('theme_code.zip')
-kospi_zip.extractall()
-
-kospi_zip.close()
-
-file_name = base_dir + "\\theme_code.mst"
-df = pd.DataFrame(columns = ['테마코드', '테마명', '종목코드'])
-
-ridx = 1
-with open(file_name, mode="r", encoding="cp949") as f:
-for row in f:
-tcode = row[0:3]
-jcode = row[-10:].rstrip()
-tname = row[3:-10].rstrip()
-df.loc[ridx] = [tcode, tname, jcode]
-ridx += 1
-
-return df
-
-
-
-df1 = get_theme_master_dataframe("d:\\pysam")
+df1 = get_theme_master_dataframe("d:\\pysam")   # pysam 임시폴더 사용
