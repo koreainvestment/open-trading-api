404ff11d4f678eed2fb384293b4edf0b8b9a9b23
KIS Developers <openapi@koreainvestment.com>
2024-05-08 17:16:06 +0900

    0508_신규API샘플추가
    - 신규(6)
    . 국내주식 예상체결
    . 국내주식 시간외 호가, 체결가, 예상체결
    . ELW 호가, 체결가
    
    - 수정사항
    . 해외주식(미국) 무료실시간호가 1호가 → 10호가 변경으로 출력함수 수정 (stockhoka_overseas_usa)
    . 해외주식(아시아) 는 그대로 무료실시간 1호가 로 출력함수명만 변경 (stockhoka_overseas_asia)

--- websocket/python/ws_domestic_overseas_all.py
+++ websocket/python/ws_domestic_overseas_all.py
@@ -109,7 +109,21 @@ def stockspurchase_domestic(data_cnt, data):
             print("%-13s[%s]" % (menu, pValue[i]))
             i += 1
 
+            
+# 국내주식예상체결 출력라이브러리
+def stockexppurchase_domestic(data_cnt, data):
+    print("============================================")
+    menulist = "유가증권단축종목코드|주식체결시간|주식현재가|전일대비구분|전일대비|등락율|가중평균주식가격|시가|고가|저가|매도호가|매수호가|거래량|누적거래량|누적거래대금|매도체결건수|매수체결건수|순매수체결건수|체결강도|총매도수량|총매수수량|체결구분|매수비율|전일거래량대비등락율|시가시간|시가대비구분|시가대비|최고가시간|고가대비구분|고가대비|최저가시간|저가대비구분|저가대비|영업일자|신장운영구분코드|거래정지여부|매도호가잔량1|매수호가잔량1|총매도호가잔량|총매수호가잔량|거래량회전율|전일동시간누적거래량|전일동시간누적거래량비율|시간구분코드|임의종료구분코드"
+    menustr = menulist.split('|')
+    pValue = data.split('^')
+    i = 0
+    for cnt in range(data_cnt):  # 넘겨받은 체결데이터 개수만큼 print 한다
+        print("### [%d / %d]" % (cnt + 1, data_cnt))
+        for menu in menustr:
+            print("%-13s[%s]" % (menu, pValue[i]))
+            i += 1
 
+            
 # 국내주식체결통보 출력라이브러리
 def stocksigningnotice_domestic(data, key, iv):
     
@@ -174,6 +188,82 @@ def stocksmarketinfo_domestic(data_cnt, data):
             print("%-13s[%s]" % (menu, pValue[i]))
             i += 1                  
 
+# 국내주식시간외체결 출력라이브러리
+def stockoverpurchase_domestic(data_cnt, data):
+    print("============================================")
+    menulist = "유가증권단축종목코드|주식체결시간|주식현재가|전일대비구분|전일대비|등락율|가중평균주식가격|시가|고가|저가|매도호가|매수호가|거래량|누적거래량|누적거래대금|매도체결건수|매수체결건수|순매수체결건수|체결강도|총매도수량|총매수수량|체결구분|매수비율|전일거래량대비등락율|시가시간|시가대비구분|시가대비|최고가시간|고가대비구분|고가대비|최저가시간|저가대비구분|저가대비|영업일자|신장운영구분코드|거래정지여부|매도호가잔량1|매수호가잔량1|총매도호가잔량|총매수호가잔량|거래량회전율|전일동시간누적거래량|전일동시간누적거래량비율"
+    menustr = menulist.split('|')
+    pValue = data.split('^')
+    i = 0
+    for cnt in range(data_cnt):  # 넘겨받은 체결데이터 개수만큼 print 한다
+        print("### [%d / %d]" % (cnt + 1, data_cnt))
+        for menu in menustr:
+            print("%-13s[%s]" % (menu, pValue[i]))
+            i += 1
+
+            
+# 국내주식시간외예상체결 출력라이브러리
+def stockoverexppurchase_domestic(data_cnt, data):
+    print("============================================")
+    menulist = "유가증권단축종목코드|주식체결시간|주식현재가|전일대비구분|전일대비|등락율|가중평균주식가격|시가|고가|저가|매도호가|매수호가|거래량|누적거래량|누적거래대금|매도체결건수|매수체결건수|순매수체결건수|체결강도|총매도수량|총매수수량|체결구분|매수비율|전일거래량대비등락율|시가시간|시가대비구분|시가대비|최고가시간|고가대비구분|고가대비|최저가시간|저가대비구분|저가대비|영업일자|신장운영구분코드|거래정지여부|매도호가잔량1|매수호가잔량1|총매도호가잔량|총매수호가잔량|거래량회전율|전일동시간누적거래량|전일동시간누적거래량비율"
+    menustr = menulist.split('|')
+    pValue = data.split('^')
+    i = 0
+    for cnt in range(data_cnt):  # 넘겨받은 체결데이터 개수만큼 print 한다
+        print("### [%d / %d]" % (cnt + 1, data_cnt))
+        for menu in menustr:
+            print("%-13s[%s]" % (menu, pValue[i]))
+            i += 1
+            
+            
+# 국내주식시간외호가 출력라이브러리
+def stockoverhoka_domestic(data):
+    """ 넘겨받는데이터가 정상인지 확인
+    print("stockoverhoka_domestic[%s]"%(data))
+    """
+    recvvalue = data.split('^')  # 수신데이터를 split '^'
+
+    print("유가증권 단축 종목코드 [" + recvvalue[0] + "]")
+    print("영업시간 [" + recvvalue[1] + "]" + "시간구분코드 [" + recvvalue[2] + "]")
+    print("======================================")
+    print("매도호가10 [%s]    잔량10 [%s]" % (recvvalue[12], recvvalue[32]))
+    print("매도호가09 [%s]    잔량09 [%s]" % (recvvalue[11], recvvalue[31]))
+    print("매도호가08 [%s]    잔량08 [%s]" % (recvvalue[10], recvvalue[30]))
+    print("매도호가07 [%s]    잔량07 [%s]" % (recvvalue[9], recvvalue[29]))
+    print("매도호가06 [%s]    잔량06 [%s]" % (recvvalue[8], recvvalue[28]))
+    print("매도호가05 [%s]    잔량05 [%s]" % (recvvalue[7], recvvalue[27]))
+    print("매도호가04 [%s]    잔량04 [%s]" % (recvvalue[6], recvvalue[26]))
+    print("매도호가03 [%s]    잔량03 [%s]" % (recvvalue[5], recvvalue[25]))
+    print("매도호가02 [%s]    잔량02 [%s]" % (recvvalue[4], recvvalue[24]))
+    print("매도호가01 [%s]    잔량01 [%s]" % (recvvalue[3], recvvalue[23]))
+    print("--------------------------------------")
+    print("매수호가01 [%s]    잔량01 [%s]" % (recvvalue[13], recvvalue[33]))
+    print("매수호가02 [%s]    잔량02 [%s]" % (recvvalue[14], recvvalue[34]))
+    print("매수호가03 [%s]    잔량03 [%s]" % (recvvalue[15], recvvalue[35]))
+    print("매수호가04 [%s]    잔량04 [%s]" % (recvvalue[16], recvvalue[36]))
+    print("매수호가05 [%s]    잔량05 [%s]" % (recvvalue[17], recvvalue[37]))
+    print("매수호가06 [%s]    잔량06 [%s]" % (recvvalue[18], recvvalue[38]))
+    print("매수호가07 [%s]    잔량07 [%s]" % (recvvalue[19], recvvalue[39]))
+    print("매수호가08 [%s]    잔량08 [%s]" % (recvvalue[20], recvvalue[40]))
+    print("매수호가09 [%s]    잔량09 [%s]" % (recvvalue[21], recvvalue[41]))
+    print("매수호가10 [%s]    잔량10 [%s]" % (recvvalue[22], recvvalue[42]))
+    print("======================================")
+    print("총매도호가 잔량        [%s]" % (recvvalue[43]))
+    print("총매도호가 잔량 증감   [%s]" % (recvvalue[54]))
+    print("총매수호가 잔량        [%s]" % (recvvalue[44]))
+    print("총매수호가 잔량 증감   [%s]" % (recvvalue[55]))
+    print("시간외 총매도호가 잔량 [%s]" % (recvvalue[45]))
+    print("시간외 총매수호가 증감 [%s]" % (recvvalue[46]))
+    print("시간외 총매도호가 잔량 [%s]" % (recvvalue[56]))
+    print("시간외 총매수호가 증감 [%s]" % (recvvalue[57]))
+    print("예상 체결가            [%s]" % (recvvalue[47]))
+    print("예상 체결량            [%s]" % (recvvalue[48]))
+    print("예상 거래량            [%s]" % (recvvalue[49]))
+    print("예상체결 대비          [%s]" % (recvvalue[50]))
+    print("부호                   [%s]" % (recvvalue[51]))
+    print("예상체결 전일대비율    [%s]" % (recvvalue[52]))
+    print("누적거래량             [%s]" % (recvvalue[53]))
+
 ### 1-2. 국내지수 ###
 
 # 국내지수체결 출력라이브러리
@@ -214,12 +304,71 @@ def indexprogramtrade_domestic(data_cnt, data):
         for menu in menustr:
             print("%-13s[%s]" % (menu, pValue[i]))
             i += 1            
+
+### 1-3. ELW ###            
             
-        
+# ELW호가 출력라이브러리
+def elwhoka_domestic(data):
+    """ 넘겨받는데이터가 정상인지 확인
+    print("elwhoka_domestic[%s]"%(data))
+    """
+    recvvalue = data.split('^')  # 수신데이터를 split '^'
+
+    print("ELW 종목코드 [" + recvvalue[0] + "]")
+    print("영업시간 [" + recvvalue[1] + "]" + "시간구분코드 [" + recvvalue[2] + "]")
+    print("======================================")
+    print("매도호가10 [%s]    잔량10 [%s]    LP매도호가잔량10 [%s]" % (recvvalue[12], recvvalue[32], recvvalue[59]))
+    print("매도호가09 [%s]    잔량09 [%s]    LP매도호가잔량09 [%s]" % (recvvalue[11], recvvalue[31], recvvalue[58]))
+    print("매도호가08 [%s]    잔량08 [%s]    LP매도호가잔량08 [%s]" % (recvvalue[10], recvvalue[30], recvvalue[57]))
+    print("매도호가07 [%s]    잔량07 [%s]    LP매도호가잔량07 [%s]" % (recvvalue[9], recvvalue[29], recvvalue[56]))
+    print("매도호가06 [%s]    잔량06 [%s]    LP매도호가잔량06 [%s]" % (recvvalue[8], recvvalue[28], recvvalue[55]))
+    print("매도호가05 [%s]    잔량05 [%s]    LP매도호가잔량05 [%s]" % (recvvalue[7], recvvalue[27], recvvalue[54]))
+    print("매도호가04 [%s]    잔량04 [%s]    LP매도호가잔량04 [%s]" % (recvvalue[6], recvvalue[26], recvvalue[53]))
+    print("매도호가03 [%s]    잔량03 [%s]    LP매도호가잔량03 [%s]" % (recvvalue[5], recvvalue[25], recvvalue[52]))
+    print("매도호가02 [%s]    잔량02 [%s]    LP매도호가잔량02 [%s]" % (recvvalue[4], recvvalue[24], recvvalue[51]))
+    print("매도호가01 [%s]    잔량01 [%s]    LP매도호가잔량01 [%s]" % (recvvalue[3], recvvalue[23], recvvalue[50]))
+    print("--------------------------------------")
+    print("매수호가01 [%s]    잔량01 [%s]    LP매수호가잔량01 [%s]" % (recvvalue[13], recvvalue[33], recvvalue[60]))
+    print("매수호가02 [%s]    잔량02 [%s]    LP매수호가잔량02 [%s]" % (recvvalue[14], recvvalue[34], recvvalue[61]))
+    print("매수호가03 [%s]    잔량03 [%s]    LP매수호가잔량03 [%s]" % (recvvalue[15], recvvalue[35], recvvalue[62]))
+    print("매수호가04 [%s]    잔량04 [%s]    LP매수호가잔량04 [%s]" % (recvvalue[16], recvvalue[36], recvvalue[63]))
+    print("매수호가05 [%s]    잔량05 [%s]    LP매수호가잔량05 [%s]" % (recvvalue[17], recvvalue[37], recvvalue[64]))
+    print("매수호가06 [%s]    잔량06 [%s]    LP매수호가잔량06 [%s]" % (recvvalue[18], recvvalue[38], recvvalue[65]))
+    print("매수호가07 [%s]    잔량07 [%s]    LP매수호가잔량07 [%s]" % (recvvalue[19], recvvalue[39], recvvalue[66]))
+    print("매수호가08 [%s]    잔량08 [%s]    LP매수호가잔량08 [%s]" % (recvvalue[20], recvvalue[40], recvvalue[67]))
+    print("매수호가09 [%s]    잔량09 [%s]    LP매수호가잔량09 [%s]" % (recvvalue[21], recvvalue[41], recvvalue[68]))
+    print("매수호가10 [%s]    잔량10 [%s]    LP매수호가잔량10 [%s]" % (recvvalue[22], recvvalue[42], recvvalue[69]))
+    print("======================================")
+    print("총매도호가 잔량        [%s]" % (recvvalue[43]))
+    print("총매수호가 잔량        [%s]" % (recvvalue[44]))
+    print("LP총매도호가잔량       [%s]" % (recvvalue[70]))
+    print("LP총매수호가잔량       [%s]" % (recvvalue[71]))
+    print("예상 체결가            [%s]" % (recvvalue[45]))
+    print("예상 체결량            [%s]" % (recvvalue[46]))
+    print("예상 체결대비부호      [%s]" % (recvvalue[47]))
+    print("예상 체결대비          [%s]" % (recvvalue[48]))
+    print("예상 체결전일대비율    [%s]" % (recvvalue[48]))
+    print("예상 거래량            [%s]" % (recvvalue[72]))
+
+    
+# ELW체결처리 출력라이브러리
+def elwpurchase_domestic(data_cnt, data):
+    print("============================================")
+    menulist = "유가증권단축종목코드|주식체결시간|주식현재가|전일대비부호|전일대비|전일대비율|가중평균주식가격|주식시가2|주식최고가|주식최저가|매도호가1|매수호가1|체결거래량|누적거래량|누적거래대금|매도체결건수|매수체결건수|순매수체결건수|체결강도|총매도수량|총매수수량|체결구분코드|매수2비율|전일거래량대비등락율|시가시간|시가2대비현재가부호|시가2대비현재가|최고가시간|최고가대비현재가부호|최고가대비현재가|최저가시간|최저가대비현재가부호|최저가대비현재가|영업일자|신장운영구분코드|거래정지여부|매도호가잔량1|매수호가잔량1|총매도호가잔량|총매수호가잔량|시간가치값|패리티|프리미엄값|기어링|손익분기비율|내재가치값|프리미엄비율|자본지지점|레버리지값|델타|감마|베가|세타|로우|HTS내재변동성|HTS이론가|거래량회전율|전일동시간누적거래량|전일동시간누적거래량비율|접근도|LP보유량|LP보유비율|LP순매도량"
+    menustr = menulist.split('|')
+    pValue = data.split('^')
+    i = 0
+    for cnt in range(data_cnt):  # 넘겨받은 체결데이터 개수만큼 print 한다
+        print("### [%d / %d]" % (cnt + 1, data_cnt))
+        for menu in menustr:
+            print("%-13s[%s]" % (menu, pValue[i]))
+            i += 1
+            
+            
 ### 2. 해외주식 ###
             
-# 해외주식호가 출력라이브러리
-def stockhoka_overseas(data):
+# 해외주식(아시아)호가 출력라이브러리
+def stockhoka_overseas_asia(data):
     """ 넘겨받는데이터가 정상인지 확인
     print("stockhoka[%s]"%(data))
     """
@@ -240,6 +389,46 @@ def stockhoka_overseas(data):
     print("매도잔량           [%s]" % (recvvalue[14]))
     print("매수잔량대비        [%s]" % (recvvalue[15]))
     print("매도잔량대비        [%s]" % (recvvalue[16]))
+
+          
+# 해외주식(미국)호가 출력라이브러리
+def stockhoka_overseas_usa(data):
+    """ 넘겨받는데이터가 정상인지 확인
+    print("stockhoka[%s]"%(data))
+    """
+    recvvalue = data.split('^')  # 수신데이터를 split '^'
+
+    print("실시간종목코드 [" + recvvalue[0] + "]" + ", 종목코드 [" + recvvalue[1] + "]")
+    print("소숫점자리수 [" + recvvalue[2] + "]")
+    print("현지일자 [" + recvvalue[3] + "]" + ", 현지시간 [" + recvvalue[4] + "]")
+    print("한국일자 [" + recvvalue[5] + "]" + ", 한국시간 [" + recvvalue[6] + "]")
+    print("======================================")    
+    print("매도호가10 [%s]    잔량10 [%s]" % (recvvalue[66], recvvalue[68]))
+    print("매도호가09 [%s]    잔량09 [%s]" % (recvvalue[60], recvvalue[62]))
+    print("매도호가08 [%s]    잔량08 [%s]" % (recvvalue[54], recvvalue[56]))
+    print("매도호가07 [%s]    잔량07 [%s]" % (recvvalue[48], recvvalue[50]))
+    print("매도호가06 [%s]    잔량06 [%s]" % (recvvalue[42], recvvalue[44]))
+    print("매도호가05 [%s]    잔량05 [%s]" % (recvvalue[36], recvvalue[38]))
+    print("매도호가04 [%s]    잔량04 [%s]" % (recvvalue[30], recvvalue[32]))
+    print("매도호가03 [%s]    잔량03 [%s]" % (recvvalue[24], recvvalue[26]))
+    print("매도호가02 [%s]    잔량02 [%s]" % (recvvalue[18], recvvalue[20]))
+    print("매도호가01 [%s]    잔량01 [%s]" % (recvvalue[12], recvvalue[14]))
+    print("--------------------------------------")
+    print("매수호가01 [%s]    잔량01 [%s]" % (recvvalue[11], recvvalue[13]))
+    print("매수호가02 [%s]    잔량02 [%s]" % (recvvalue[17], recvvalue[19]))
+    print("매수호가03 [%s]    잔량03 [%s]" % (recvvalue[23], recvvalue[25]))
+    print("매수호가04 [%s]    잔량04 [%s]" % (recvvalue[29], recvvalue[31]))
+    print("매수호가05 [%s]    잔량05 [%s]" % (recvvalue[35], recvvalue[37]))
+    print("매수호가06 [%s]    잔량06 [%s]" % (recvvalue[41], recvvalue[43]))
+    print("매수호가07 [%s]    잔량07 [%s]" % (recvvalue[47], recvvalue[49]))
+    print("매수호가08 [%s]    잔량08 [%s]" % (recvvalue[53], recvvalue[55]))
+    print("매수호가09 [%s]    잔량09 [%s]" % (recvvalue[59], recvvalue[61]))
+    print("매수호가10 [%s]    잔량10 [%s]" % (recvvalue[65], recvvalue[67]))
+    print("======================================")
+    print("매수총 잔량        [%s]" % (recvvalue[7]))
+    print("매수총잔량대비      [%s]" % (recvvalue[9]))
+    print("매도총 잔량        [%s]" % (recvvalue[8]))
+    print("매도총잔략대비      [%s]" % (recvvalue[10]))
 
 
 # 해외주식체결처리 출력라이브러리
@@ -600,14 +789,20 @@ async def connect():
     
     # 원하는 호출을 [tr_type, tr_id, tr_key] 순서대로 리스트 만들기
     
-    ### 1-1. 국내주식 호가, 체결가, 체결통보 ### # 모의투자 국내주식 체결통보: H0STCNI9
-    # code_list = [['1','H0STASP0','005930'],['1','H0STCNT0','005930'],['1','H0STCNI0','HTS ID를 입력하세요']]
+    ### 1-1. 국내주식 호가, 체결가, 예상체결, 체결통보 ### # 모의투자 국내주식 체결통보: H0STCNI9
+    # code_list = [['1','H0STASP0','005930'],['1','H0STCNT0','005930'],['1', 'H0STANC0', '005930'],['1','H0STCNI0','HTS ID를 입력하세요']]
     
     ### 1-2. 국내주식 실시간회원사, 실시간프로그램매매, 장운영정보 ###
     # code_list = [['1', 'H0STMBC0', '005930'], ['1', 'H0STPGM0', '005930'], ['1', 'H0STMKO0', '005930']]
     
-    ### 1-3. 국내지수 체결, 예상체결, 실시간프로그램매매 ###
+    ### 1-3. 국내주식 시간외 호가, 체결가, 예상체결 ###
+    # code_list = [['1','H0STOAA0','005930'],['1','H0STOUP0','005930'],['1', 'H0STOAC0', '005930']]
+    
+    ### 1-4. 국내지수 체결, 예상체결, 실시간프로그램매매 ###
     # code_list = [['1', 'H0UPCNT0', '0001'], ['1', 'H0UPANC0', '0001'], ['1', 'H0UPPGM0', '0001']]
+    
+    ### 1-5. ELW 호가, 체결가 ###
+    # code_list = [['1', 'H0EWASP0', '58J297'],['1', 'H0EWCNT0', '58J297']]
     
     ### 2-1. 해외주식(미국) 호가, 체결가, 체결통보 ###
     # code_list = [['1','HDFSASP0','DNASAAPL'],['1','HDFSCNT0','DNASAAPL'],['1','H0GSCNI0','HTS ID를 입력하세요']]
@@ -635,12 +830,13 @@ async def connect():
     #              ['1','HDFFF2C0','HTS ID를 입력하세요']] # 해외선물옵션 체결통보
     
     ### 1+2+3+4. 국내주식, 해외주식(미국), 해외주식(아시아), 국내 지수선물옵션, 국내 상품선물, 국내 주식선물옵션, 해외선물옵션 호가, 체결가, 체결통보 ###
-    code_list = [['1','H0STASP0','005930'],['1','H0STCNT0','005930'],['1','H0STCNI0','HTS ID를 입력하세요'],
-                 ['1','HDFSASP0','DNASAAPL'],['1','HDFSCNT0','DNASAAPL'],['1','H0GSCNI0','HTS ID를 입력하세요'],
+    code_list = [['1','H0STASP0','005930'],['1','H0STCNT0','005930'],['1', 'H0STANC0', '005930'],['1','H0STCNI0','HTS ID를 입력하세요'],
+                 ['1','HDFSASP0','DNASAAPL'],['1','HDFSCNT0','DNASAAPL'],
                  ['1','HDFSASP1','DHKS00003'],['1','HDFSCNT0','DHKS00003'],['1','H0GSCNI0','HTS ID를 입력하세요'],
                  ['1','H0IFASP0','101T12'],['1','H0IFCNT0','101T12'],['1','H0IOASP0','201T11317'],['1','H0IOCNT0','201T11317'], ['1','H0CFASP0','175T11'],['1','H0CFCNT0','175T11'],['1', 'H0ZFCNT0', '111V06'], ['1', 'H0ZFASP0', '111V06'],['1', 'H0ZOCNT0', '211V05059'], ['1', 'H0ZOASP0', '211V05059'],['1','H0IFCNI0','HTS ID를 입력하세요'],
                  ['1','HDFFF020','FCAZ22'],['1','HDFFF010','FCAZ22'],['1','HDFFF020','OESH23 C3900'],['1','HDFFF010','OESH23 C3900'],['1','HDFFF2C0','HTS ID를 입력하세요']]
     
+
     senddata_list=[]
     
     for i,j,k in code_list:
@@ -667,15 +863,37 @@ async def connect():
                     trid0 = recvstr[1]
 
                     if trid0 == "H0STASP0":  # 주식호가tr 일경우의 처리 단계
-                        print("#### 주식호가 ####")
+                        print("#### 국내주식 호가 ####")
                         stockhoka_domestic(recvstr[3])
                         # await asyncio.sleep(0.2)
 
                     elif trid0 == "H0STCNT0":  # 주식체결 데이터 처리
-                        print("#### 주식체결 ####")
+                        print("#### 국내주식 체결 ####")
                         data_cnt = int(recvstr[2])  # 체결데이터 개수
                         stockspurchase_domestic(data_cnt, recvstr[3])
                         # await asyncio.sleep(0.2)
+                    
+                    elif trid0 == "H0STANC0":  # 국내주식 예상체결 데이터 처리
+                        print("#### 국내주식 예상체결 ####")
+                        data_cnt = int(recvstr[2])  # 체결데이터 개수
+                        stockexppurchase_domestic(data_cnt, recvstr[3])
+                        # await asyncio.sleep(0.2)  
+                    
+                    elif trid0 == "H0STOUP0":  # 국내주식 시간외체결 데이터 처리
+                        print("#### 국내주식 시간외체결 ####")
+                        data_cnt = int(recvstr[2])  # 체결데이터 개수
+                        stockoverpurchase_domestic(data_cnt, recvstr[3])
+                        # await asyncio.sleep(0.2) 
+
+                    elif trid0 == "H0STOAA0":  # 국내주식 시간외호가 데이터 처리
+                        print("#### 국내주식 시간외호가 ####")
+                        stockoverhoka_domestic(recvstr[3])
+                        # await asyncio.sleep(0.2)                        
+                        
+                    elif trid0 == "H0STOAC0":  # 국내주식 시간외예상체결데이터 처리
+                        print("#### 국내주식 시간외예상체결 ####")
+                        data_cnt = int(recvstr[2])  # 체결데이터 개수
+                        stockoverexppurchase_domestic(data_cnt, recvstr[3])
                         
                     elif trid0 == "H0STMBC0":  # 국내주식 실시간회원사 데이터 처리
                         print("#### 국내주식 실시간회원사 ####")
@@ -711,15 +929,26 @@ async def connect():
                         data_cnt = int(recvstr[2])  # 체결데이터 개수
                         indexprogramtrade_domestic(data_cnt, recvstr[3])
                         # await asyncio.sleep(0.2)    
+                    
+                    elif trid0 == "H0EWCNT0":  # 주식옵션 체결 데이터 처리
+                        print("#### ELW 체결 ####")
+                        data_cnt = int(recvstr[2])  # 체결데이터 개수
+                        elwpurchase_domestic(data_cnt, recvstr[3])
+                        # await asyncio.sleep(0.2) 
+
+                    elif trid0 == "H0EWASP0":  # 주식옵션 호가 데이터 처리
+                        print("#### ELW 호가 ####")
+                        elwhoka_domestic(recvstr[3])
+                        # await asyncio.sleep(0.2)   
 
                     elif trid0 == "HDFSASP0":  # 해외주식호가tr 일경우의 처리 단계
                         print("#### 해외(미국)주식호가 ####")
-                        stockhoka_overseas(recvstr[3])
+                        stockhoka_overseas_usa(recvstr[3])
                         # await asyncio.sleep(0.2)
 
                     elif trid0 == "HDFSASP1":  # 해외주식호가tr 일경우의 처리 단계
                         print("#### 해외(아시아)주식호가 ####")
-                        stockhoka_overseas(recvstr[3])
+                        stockhoka_overseas_asia(recvstr[3])
                         # await asyncio.sleep(0.2)
 
                     elif trid0 == "HDFSCNT0":  # 해외주식체결 데이터 처리
