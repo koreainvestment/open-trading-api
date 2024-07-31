5bfec8190fc0b8dd230145b85083bb495630d2d3
KIS Developers <87407853+koreainvestment@users.noreply.github.com>
2024-07-31 15:48:51 +0900

    [해외선물옵션] 실시간(웹소켓) 체결/호가/계좌주문내역발생/계좌체결발생+REST 호출 샘플
    [해외선물옵션] 실시간(웹소켓) 체결/호가/계좌주문내역발생/계좌체결발생+REST 호출 샘플

--- Sample01/kis_ovrseafuopt_ws.py
+++ Sample01/kis_ovrseafuopt_ws.py
@@ -355,14 +355,16 @@ def _dparse(data):
 
         if tr_id in (KIS_WSReq.CONTRACT, KIS_WSReq.BID_ASK):  # 실시간체결, 실시간호가
             dp_ = pd.read_csv(StringIO(d1[3]), header=None, sep='^', names=hcols, dtype=object)  # 수신데이터 parsing
-            # print(dp_)  # 실시간체결, 실시간호가 수신 데이터 파싱 결과 확인
+            
+            print(dp_)  # 실시간체결, 실시간호가 수신 데이터 파싱 결과 확인
+            
             dp_['TICK_HOUR'] = _today__ + dp_['TICK_HOUR']    # 수신시간
             dp_['TICK_HOUR'] = pd.to_datetime(dp_['TICK_HOUR'], format='%Y%m%d%H%M%S', errors='coerce')
         else:  # 실시간 계좌체결발생통보는 암호화되어서 수신되므로 복호화 과정이 필요
             dp_ = pd.read_csv(StringIO(aes_cbc_base64_dec(_ekey, _iv, d1[3])), header=None, sep='^', names=hcols,  # 수신데이터 parsing 및 복호화
                               dtype=object)
 
-            # print(dp_)  # 실시간 계좌체결발생통보 수신 파싱 결과 확인
+            print(dp_)  # 실시간 계좌체결발생통보 수신 파싱 결과 확인
 
             if __DEBUG__: print(f'***EXECUTED CCLDNOTICE [{dp_.to_string(header=False, index=False)}]')
 
@@ -449,7 +451,7 @@ def _get_sys_resp(data):
 
 
 def on_data(ws, data, resp_type, data_continu):
-    print(f"On data => {resp_type}, {data_continu}, {data}") #return only 1, True
+    # print(f"On data => {resp_type}, {data_continu}, {data}") #return only 1, True
     pass
 
 
