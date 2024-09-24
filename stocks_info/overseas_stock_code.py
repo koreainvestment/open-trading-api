a5ed176e833f00c281769e8365bfac821c0df059
kghoon <kang@ghoon.net>
2024-09-24 16:33:27 +0900

    fix: 파일 경로 조합 시 os.path.join() 사용

--- stocks_info/overseas_stock_code.py
+++ stocks_info/overseas_stock_code.py
@@ -26,18 +26,18 @@ base_dir = os.getcwd()
 def get_overseas_master_dataframe(base_dir,val):
 
     ssl._create_default_https_context = ssl._create_unverified_context
-    urllib.request.urlretrieve(f"https://new.real.download.dws.co.kr/common/master/{val}mst.cod.zip", base_dir + f"\\{val}mst.cod.zip")
+    urllib.request.urlretrieve(f"https://new.real.download.dws.co.kr/common/master/{val}mst.cod.zip", os.path.join(base_dir, f"{val}mst.cod.zip"))
     os.chdir(base_dir)
 
     overseas_zip = zipfile.ZipFile(f'{val}mst.cod.zip')
     overseas_zip.extractall()
     overseas_zip.close()
 
-    file_name = base_dir + f"\\{val}mst.cod"
+    file_name = os.path.join(base_dir, f"{val}mst.cod")
     columns = ['National code', 'Exchange id', 'Exchange code', 'Exchange name', 'Symbol', 'realtime symbol', 'Korea name', 'English name', 'Security type(1:Index,2:Stock,3:ETP(ETF),4:Warrant)', 'currency', 'float position', 'data type', 'base price', 'Bid order size', 'Ask order size', 'market start time(HHMM)', 'market end time(HHMM)', 'DR 여부(Y/N)', 'DR 국가코드', '업종분류코드', '지수구성종목 존재 여부(0:구성종목없음,1:구성종목있음)', 'Tick size Type', '구분코드(001:ETF,002:ETN,003:ETC,004:Others,005:VIX Underlying ETF,006:VIX Underlying ETN)','Tick size type 상세']
     
     print(f"Downloading...{val}mst.cod")
-    df = pd.read_table(base_dir+f"\\{val}mst.cod",sep='\t',encoding='cp949')
+    df = pd.read_table(os.path.join(base_dir, f"{val}mst.cod"), sep='\t',encoding='cp949')
     df.columns = columns
     df.to_excel(f'{val}_code.xlsx',index=False)  # 현재 위치에 엑셀파일로 저장
 
