7d3aa2e79856033a5d9f137a0dc3a4dc9218be7f
kho126942-a11y <kho126942@gmail.com>
2026-06-03 15:32:55 +0900

    Add configuration for trading application
    Added environment variable configurations and trading parameters.

--- samsung_auto_trader/config.py
+++ samsung_auto_trader/config.py
@@ -1,1 +1,15 @@
+import os
 
+APP_KEY = os.getenv("GH_APPKEY")
+APP_SECRET = os.getenv("GH_APPSECRET")
+ACCOUNT = os.getenv("GH_ACCOUNT")
+
+SYMBOL = "005930"
+
+TRADING_START = "09:10"
+TRADING_END = "15:30"
+
+POLL_INTERVAL_SECONDS = 60
+ORDER_PRICE_GAP = 1000
+
+MOCK_BASE_URL = "https://openapivts.koreainvestment.com:29443"
