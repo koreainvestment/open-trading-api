ca3642c8c6f67274e91d1fdfc4b922a4c02a2714
kho126942-a11y <kho126942@gmail.com>
2026-06-03 18:22:34 +0900

    Add logger setup function for samsung_auto_trader

--- samsung_auto_trader/logger.py
+++ samsung_auto_trader/logger.py
@@ -1,1 +1,16 @@
+import logging
 
+
+def setup_logger() -> logging.Logger:
+    logger = logging.getLogger("samsung_auto_trader")
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "[%(asctime)s] %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
