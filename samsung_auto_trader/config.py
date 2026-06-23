ecf5c56072074361ffc8f01ab9acdefd77e563b1
kho126942-a11y <kho126942@gmail.com>
2026-06-23 11:21:31 +0900

    Update config.py

--- samsung_auto_trader/config.py
+++ samsung_auto_trader/config.py
@@ -5,9 +5,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 
 #GitHub Codespaces Secret 또는 환경변수에서 API key, secret, 계좌번호를 불러온다.
-APP_KEY = os.getenv("GH_APPKEY")
-APP_SECRET = os.getenv("GH_APPSECRET")
-ACCOUNT = os.getenv("GH_ACCOUNT")
+APP_KEY = "PSzJDY1n4UEorZOCtijH5BoKKSCldXsr25fU"
+APP_SECRET = esK0A1W5CZY3IrkBYkC0Bx2KJ/Lo3tcoFMH/QHuxAl7l8dHA5zcj/qDJa7+mSR43MiYE7u475e3ZAm5NyVxM6OqOadFutx3aHtyU0sctCqsdjjkzp2+bHDY4+0Yu2GZe8yn6KG4rGX40wkgE/5FK4AK70HFiiSHUveO8XNCX/7ThcA/oRno=
+ACCOUNT = "5017750401"
 
 if not APP_KEY:
     raise RuntimeError("GH_APPKEY environment variable is missing.")
